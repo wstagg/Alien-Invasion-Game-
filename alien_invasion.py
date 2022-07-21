8d3ed2732cf206513e2a4616ce9a2b8639859ef2
[Wesley Stagg] <[wes.stagg@gmail.com]>
2022-07-21 16:29:02 +0100

    Added scoreboard and highscore.

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -27,8 +28,9 @@ class AlienInvasion:
 
 		pygame.display.set_caption("Alien Invasion")
 
-		# Create an instance to store game statistics.
+		# Create and instance to store the game statistics and create scoreboard.
 		self.stats = GameStats(self)
+		self.sb = Scoreboard(self)
 
 		self.ship = Ship(self)
 		self.bullets = pygame.sprite.Group()
@@ -113,6 +115,7 @@ class AlienInvasion:
 			self.settings.initialize_dynamic_settings()
 			self.stats.reset_stats()
 			self.stats.game_active = True 
+			self.sb.prep_score()
 
 			# get rid of any remaining aliens and bullets.
 			self.aliens.empty()
@@ -166,6 +169,12 @@ class AlienInvasion:
 		"""Respond to bullet-alien collisions."""
 		# Remove any bullets and aliens that have collided.
 		collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+		if collisions:
+			for aliens in collisions.values():
+				self.stats.score += self.settings.alien_points * len(aliens)
+			self.sb.prep_score()
+			self.sb.check_high_score()   
 		
 		if not self.aliens:
 			# Destroy existing bullets and create new fleet.
@@ -215,7 +224,6 @@ class AlienInvasion:
 				self._ship_hit()
 				break
 
-
 	def _update_screen(self):
 		"""Update images on the screen and flip to the new screen"""
 		self.screen.fill(self.settings.bg_color) # Redraw the screen during each pass through the loop.
@@ -223,6 +231,9 @@ class AlienInvasion:
 		for bullet in self.bullets.sprites():
 			bullet.draw_bullet()
 		self.aliens.draw(self.screen)
+
+		# Draw thr score information.
+		self.sb.show_score()
 
 		# Draw the play button if the game is inactive.
 		if not self.stats.game_active:
--- game_stats.py
+++ game_stats.py
@@ -8,8 +8,12 @@ class GameStats:
 		# Start alien invasion in an inactive state. 
 		self.game_active = False 
 
+		# Hight socre should never be reset.
+		self.high_score = 0
+
 	def reset_stats(self):
 		"""Initialize statistics."""
 		self.ships_left = self.settings.ship_limit
+		self.score = 0
 
 		
